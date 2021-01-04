895394c7be0b5fd00b712189cc6ce2ece0181249
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-01-04 23:04:36 +0200

    Size extension's chart

--- Project/main.py
+++ Project/main.py
@@ -1,92 +1,64 @@
 import os
 import collections
 import matplotlib.pyplot as plt;
-import plotly.graph_objects as go
-
-plt.rcdefaults()
-import numpy as np
-
-'''
-def size(path):
-	size = os.path.getsize(path) #Size in bytes
-	return size
-'''
 
 
 def dir(path):
     nrDir = 0  # Numar Directoare
-    sizeDir = 0  # Dimensiune Directoare
     for (root, directories, files) in os.walk(path):
         for dirs in directories:
-            full_fileName = os.path.join(root, dirs)
             nrDir += 1
-            # skip if it is symbolic link
-            if not os.path.islink(full_fileName):
-                sizeDir += os.path.getsize(full_fileName)
     print("Number of Directories: " + str(nrDir))
-    print("Size of Directories: " + str(sizeDir) + " bytes")
-    return nrDir, sizeDir
-
+    return nrDir
 
 def files(path):
     nrFiles = 0  # Numar Fisiere
-    sizeFiles = 0  # Dimensiune Fisiere
-    nrExtensions = collections.Counter()  # Numarul de aparitii al unei Extensii
+    nrExtensions = collections.Counter()  # Numarul de aparitii al unei extensii
+    sizeFiles = collections.Counter()  # Dimensiunea pe care o ocupa o extensie
     for (root, directories, files) in os.walk(path):
         for fileName in files:
             full_fileName = os.path.join(root, fileName)
             nrFiles += 1
-            # skip if it is symbolic link
-            if not os.path.islink(full_fileName):
-                sizeFiles += os.path.getsize(full_fileName)
             name, ext = os.path.splitext(fileName)
             nrExtensions[ext] += 1
+            sizeFiles[ext] += os.path.getsize(full_fileName)
     # print (full_fileName)
     print("Number of Files: " + str(nrFiles))
-    print("Size of Files: " + str(sizeFiles) + " bytes")
+    print("Size of Files: " + str(sizeFiles))
     print(nrExtensions.keys())
     print(nrExtensions.values())
+    print(sizeFiles.values())
     return nrFiles, sizeFiles, nrExtensions
 
-
 def partition(path):
-    nrDir, sizeDir = dir(path)
+    nrDir = dir(path)
     nrFiles, sizeFiles, nrExtensions = files(path)
 
-    # Data to plot
-    labels = nrExtensions.keys()
-    print("Keys:" + str(labels))
-    sizes = nrExtensions.values()
+    fig, (ax1, ax2) = plt.subplots(1, 2)  # Separate fig in 2 subplots
+    fig.suptitle('Proportion of each type of file', fontweight="bold")
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    # -------------------Subplot 1: piechart for number
+    # Data to plot
+    labels = nrExtensions.keys()  # Extensions names
+    sizes = nrExtensions.values()  # Number of occurences for each extension
 
     ax1.pie(sizes, labels=labels,
             autopct='%1.1f%%', shadow=True, startangle=140)
-
-    ax1.set_title('Partitions',
-                  fontweight="bold")
+    ax1.set_title('Number proportion')
     plt.axis('equal')
-    fig.suptitle('Partition-statistics')
 
-    # Sublot 2
+    # -------------------Subplot 2: piechart for size
+    # Data to plot
+    labels = nrExtensions.keys()  # Extensions names
+    sizes = sizeFiles.values()  # Size of each extension
 
-    ax2.set_axis_off()
-
-    """"
-    table = ax2.table(
-        cellText=[[nrExtensions.keys()],[nrExtensions.values()]],
-        colLabels=("Extensions", "Number of Occurences"),
-        cellLoc='center',
-        loc = 'upper left'
-    )
-    """
-
-    ax2.set_title('Number of files',
-                 fontweight="bold")
+    ax2.pie(sizes, labels=labels,
+            autopct='%1.1f%%', shadow=True, startangle=140)
+    ax2.set_title('Size proportion')
+    plt.axis('equal')
     plt.show()
 
-
 # give partition
-#partition("C:")
-#partition("C:\\test")
-partition("C:\\Jocuri")
+# partition("C:")
+partition("C:\\test")
+#partition("C:\\Jocuri")
