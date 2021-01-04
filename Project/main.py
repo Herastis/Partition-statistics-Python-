73f46e82290f6fc75b66a01446623c5de24e0860
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-01-04 23:49:57 +0200

    Added number of files and directories to chart

--- Project/main.py
+++ Project/main.py
@@ -2,7 +2,6 @@ import os
 import collections
 import matplotlib.pyplot as plt;
 
-
 def dir(path):
     nrDir = 0  # Numar Directoare
     for (root, directories, files) in os.walk(path):
@@ -22,12 +21,9 @@ def files(path):
             name, ext = os.path.splitext(fileName)
             nrExtensions[ext] += 1
             sizeFiles[ext] += os.path.getsize(full_fileName)
-    # print (full_fileName)
     print("Number of Files: " + str(nrFiles))
-    print("Size of Files: " + str(sizeFiles))
-    print(nrExtensions.keys())
-    print(nrExtensions.values())
-    print(sizeFiles.values())
+    print("Number of extensions: " + str(nrExtensions))
+    print("Size of extensions in bytes: " + str(sizeFiles))
     return nrFiles, sizeFiles, nrExtensions
 
 def partition(path):
@@ -35,19 +31,20 @@ def partition(path):
     nrFiles, sizeFiles, nrExtensions = files(path)
 
     fig, (ax1, ax2) = plt.subplots(1, 2)  # Separate fig in 2 subplots
-    fig.suptitle('Proportion of each type of file', fontweight="bold")
+    #Number of directories and files
+    fig.suptitle("Number of directories: " + str(nrDir) + '\n' + "Number of files: " + str(nrFiles), fontweight="bold", color='blue')
 
-    # -------------------Subplot 1: piechart for number
+    # -------------------Subplot 1: Pie chart for number
     # Data to plot
     labels = nrExtensions.keys()  # Extensions names
-    sizes = nrExtensions.values()  # Number of occurences for each extension
+    sizes = nrExtensions.values()  # Number of occurrences for each extension
 
     ax1.pie(sizes, labels=labels,
             autopct='%1.1f%%', shadow=True, startangle=140)
     ax1.set_title('Number proportion')
     plt.axis('equal')
 
-    # -------------------Subplot 2: piechart for size
+    # -------------------Subplot 2: Pie chart for size
     # Data to plot
     labels = nrExtensions.keys()  # Extensions names
     sizes = sizeFiles.values()  # Size of each extension
